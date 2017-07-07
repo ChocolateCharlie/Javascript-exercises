9de056869c9602831b3748e89e0a10c1c66bd6da
ChocolateCharlie <aurore.amrit@gmail.com>
2017-07-07 19:43:34 +0200

    Complete chapter eight

---
+++ OpenClassrooms/Apprenez-a-coder-avec-Javascript/chapitre_8/js/ex_1.js
@@ -0,0 +1,33 @@
+var Chien = {
+	init: function (nom, race, taille)
+	{
+		this.nom = nom;
+		this.race = race;
+		this.taille = taille;
+	},
+
+	aboyer: function()
+	{
+		if (this.taille < 50)
+		{
+			return ("Kaii ! Kaii !");
+		}
+		else
+		{
+			return ("Grr ! Grrr !");
+		}
+	}
+};
+
+
+
+var crokdur = Object.create(Chien);
+crokdur.init("Crokdur", "mâtin de Naples", 75);
+console.log(crokdur.nom + " est un " + crokdur.race + " mesurant " + crokdur.taille + " cm");
+console.log("Tiens, un chat ! " + crokdur.nom + " aboie : " + crokdur.aboyer());
+
+var pupuce = Object.create(Chien);
+pupuce.init("Pupuce", "bichon", 22);
+console.log(pupuce.nom + " est un " + pupuce.race + " mesurant " + pupuce.taille + " cm");
+console.log("Tiens, un chat ! " + pupuce.nom + " aboie : " + pupuce.aboyer());
+
